7798b44037f64c85aa79b52dee0aeea9c2f4c9ee
Edward Cooke <edward@frakkingsweet.com>
2023-09-30 13:29:08 -0600

    Added server option to dig healthcheck

--- healthcheck-dig.sh
+++ healthcheck-dig.sh
@@ -10,12 +10,14 @@ do
     [ "$1" == "--exposed-ip" ] && EXPOSEDIP=$2 && shift
     [ "$1" == "--local-ip" ] && LOCALIP=$2 && shift
     [ "$1" == "--hostname" ] && HOSTNAME=$2 && shift
+    [ "$1" == "--server" ] && SERVER=$2 && shift
     shift
 done
 
 [ -z $EXPOSEDIP ] && SHOWHELP=1 && echo -e "${RED}Exposed IP not set${NC}"
 [ -z $LOCALIP ] && SHOWHELP=1 && echo -e "${RED}Local IP not set${NC}"
 [ -z $HOSTNAME ] && SHOWHELP=1 && echo -e "${RED}Hostname not set${NC}"
+[ -z $SERVER ] && SERVER="localhost"
 
 if [ "$SHOWHELP" == "1" ]
 then
@@ -23,12 +25,18 @@ then
     echo '--exposed-ip <EXPOSED IP> (required)'
     echo '--local-ip <LOCAL IP ADDRESS> (required)'
     echo '--hostname <HOSTNAME> (required)'
+    echo '--server <DNS SERVER> (defaults to localhost)'
     exit 1 
 fi
 
+echo "EXPOSEDIP=$EXPOSEDIP"
+echo "LOCALIP=$LOCALIP"
+echo "HOSTNAME=$HOSTNAME"
+echo "SERVER=$SERVER"
+
 while `true`
 do
-    dig $HOSTNAME @localhost 1> /dev/null 2> /dev/null
+    dig $HOSTNAME @$SERVER 1> /dev/null 2> /dev/null
     if [ $? == 0 ]
     then
         if [ $UP == 0 ]
